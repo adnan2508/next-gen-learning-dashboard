1d9c1ee3d8e5dd6a330cbc177c88b4987052b044
adnan2508 <adnandiu2800@gmail.com>
2026-06-04 00:38:37 +0600

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Next gen learning Dashboard",
+  description: "Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>This is title</h1>
+      </main>
+    </div>
+  );
+}
